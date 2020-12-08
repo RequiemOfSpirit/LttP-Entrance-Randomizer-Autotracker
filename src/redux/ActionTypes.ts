1b2e576d016d2e6014c60ad8ddcc74fb909014d3
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2020-12-08 09:48:45 -0800

    Remove string values in ActionType enum

--- src/redux/ActionTypes.ts
+++ src/redux/ActionTypes.ts
@@ -1,14 +1,14 @@
 export enum ActionType {
   // Game info related
-  ADD_ENTRANCE_LINKS = "AddEntranceLinks",
-  UPDATE_INVENTORY = "UpdateInventory",
+  ADD_ENTRANCE_LINKS,
+  UPDATE_INVENTORY,
 
   // Usb2Snes server related
-  UPDATE_DEVICE_LIST = "UpdateDeviceList",
-  UPDATE_CONNECTED_DEVICE = "UpdateConnectedDevice",
-  UPDATE_SERVER_CONNECTION_STATUS = "UpdateServerConnectionStatus",
-  RESET_DEVICE_DATA = "ResetDeviceData",
+  UPDATE_DEVICE_LIST,
+  UPDATE_CONNECTED_DEVICE,
+  UPDATE_SERVER_CONNECTION_STATUS,
+  RESET_DEVICE_DATA,
 
   // Other
-  UPDATE_APP_SETTINGS = "UpdateAppSettings"
+  UPDATE_APP_SETTINGS
 }
